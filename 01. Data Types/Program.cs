285c046597252d55af9f991ca5d325c278757363
Lyubomir Lalov <ldlalov@yahoo.com>
2022-02-05 14:07:59 +0200

    Add project files.

---
+++ 01. Data Types/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "int":
+                    Console.WriteLine(Int());
+                        break;
+                case "real":
+                    Console.WriteLine(Real());
+                        break;
+                case "string":
+                    Console.WriteLine(String());
+                        break;
+            }
+        }
+      static int Int()
+    {
+            int num = int.Parse(Console.ReadLine());
+            num *= 2;
+            return num;
+    }
+      static string Real()
+    {
+            double num = double.Parse(Console.ReadLine());
+            num *= 1.50;
+            return $"{num:f2}";
+
+        }
+        static string String()
+    {
+            string str = Console.ReadLine();
+            return $"${str}$";
+    }
+
+    }
+}
---
+++ Methods - More Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods___More_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
