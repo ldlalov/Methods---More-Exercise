7b83ba61a10c1a80c85ab17a6bcb0b4d49acb50a
Любомир Лалов <ldlalov@hotmail.com>
2022-02-07 12:04:31 +0200

    Full Ready

--- 04. Tribonacci Sequence/Program.cs
+++ 04. Tribonacci Sequence/Program.cs
@@ -6,24 +6,30 @@ namespace _04._Tribonacci_Sequence
     {
         static void Main(string[] args)
         {
-            int n1 = 0, n2 = 1, n3 = 1, n4 = 0, i, number;
-            number = int.Parse(Console.ReadLine());
-            if (number>1)
+            int number = int.Parse(Console.ReadLine());
+            Tribonacci(number);
+
+        }
+        static void Tribonacci(int number)
+        {
+            int n1 = 0, n2 = 1, n3 = 1, i;
+            if (number > 1)
             {
-            Console.Write($"{n2} {n3}");
-            for (i = 2; i < number; ++i)
-            {
-                n4 = n1 + n2 + n3;
-                n1 = n2;
-                n2 = n3;
-                n3 = n4;
-                Console.Write($" {n4}");
-            }
+                Console.Write($"{n2} {n3}");
+                for (i = 2; i < number; ++i)
+                {
+                    int n4 = n1 + n2 + n3;
+                    n1 = n2;
+                    n2 = n3;
+                    n3 = n4;
+                    Console.Write($" {n4}");
+                }
             }
             else
             {
                 Console.WriteLine(number);
             }
+
         }
     }
 }
--- 05. Multiplication Sign/Program.cs
+++ 05. Multiplication Sign/Program.cs
@@ -6,7 +6,41 @@ namespace _05._Multiplication_Sign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            PositiveNegativeZero(num1, num2, num3);
+        }
+        static void PositiveNegativeZero(int num1, int num2, int num3)
+        {
+            if (num1==0 || num2==0 || num3==0)
+            {
+                Console.WriteLine("zero");
+            }
+            else
+            {
+                NegativePositive(num1, num2, num3);
+            }
+        }
+        static void NegativePositive(int num1, int num2, int num3)
+        {
+            int[] nums = { num1, num2, num3 };
+            int counter = 0;
+            foreach (int num in nums)
+            {
+                if (num< 0)
+                {
+                    counter++;
+                }
+            }
+            if (counter%2 == 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
         }
     }
 }
